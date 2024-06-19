1a3dffdb346e80585c37bff028bf718f0e15ace7
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-06-20 02:09:49 +0800

    Create Spring Boot project

---
+++ src/main/java/ntou/cse/ghchcloudserver/GhchCloudServerApplication.java
@@ -0,0 +1,13 @@
+package ntou.cse.ghchcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhchCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhchCloudServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ntou/cse/ghchcloudserver/GhchCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.cse.ghchcloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhchCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
