05f9f1fd0e84c24a249951461e5470cd36a846d6
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-06-30 13:37:16 +0800

    Add basic endpoints for TeamRepo

---
+++ src/main/java/ntou/cse/ghchcloudserver/teamrepo/TeamRepoRepository.java
@@ -0,0 +1,11 @@
+package ntou.cse.ghchcloudserver.teamrepo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TeamRepoRepository extends MongoRepository<TeamRepo, String> {
+    List<TeamRepo> findAllByTeamName(String teamName);
+}
