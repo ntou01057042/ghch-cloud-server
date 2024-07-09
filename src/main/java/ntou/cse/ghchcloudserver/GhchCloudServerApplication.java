2ca9918369448ece0eb77c622af3aea4d0af8b39
ouoxii <yuchuner317@gmail.com>
2024-07-09 13:45:15 +0800

    fix: header-location-cors

--- src/main/java/ntou/cse/ghchcloudserver/GhchCloudServerApplication.java
+++ src/main/java/ntou/cse/ghchcloudserver/GhchCloudServerApplication.java
@@ -2,6 +2,9 @@ package ntou.cse.ghchcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GhchCloudServerApplication {
@@ -10,4 +13,17 @@ public class GhchCloudServerApplication {
         SpringApplication.run(GhchCloudServerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .exposedHeaders("Location");
+            }
+        };
+    }
 }
