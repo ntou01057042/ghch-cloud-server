f087dcf5d9c67ce16989e372d9725c3c8a35dcf2
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-07-10 00:17:12 +0800

    Create WebConfig and delete all @CrossOrigin

--- src/main/java/ntou/cse/ghchcloudserver/team/TeamController.java
+++ src/main/java/ntou/cse/ghchcloudserver/team/TeamController.java
@@ -8,7 +8,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
