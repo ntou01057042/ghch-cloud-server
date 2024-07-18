780114dad7f90083813e6f274d3c8e8b413ab4e5
ntou01057042 <134361473+ntou01057042@users.noreply.github.com>
2024-07-18 23:52:45 +0800

    Add RepoInvitation rest enpoints

---
+++ src/main/java/ntou/cse/ghchcloudserver/repoinvitation/RepoInvitationController.java
@@ -0,0 +1,39 @@
+package ntou.cse.ghchcloudserver.repoinvitation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/repo-invitations")
+public class RepoInvitationController {
+
+    private final RepoInvitationRepository repoInvitationRepository;
+
+    public RepoInvitationController(RepoInvitationRepository repoInvitationRepository) {
+        this.repoInvitationRepository = repoInvitationRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createRepoInvitation(@RequestBody RepoInvitation newRepoInvitationRequest, UriComponentsBuilder ucb) {
+        RepoInvitation savedRepoInvitation = repoInvitationRepository.save(newRepoInvitationRequest);
+        URI locationOfNewRepoInvitation = ucb
+                .path("/repo-invitations/{id}")
+                .buildAndExpand(savedRepoInvitation.getId())
+                .toUri();
+        return ResponseEntity.created(locationOfNewRepoInvitation).build();
+    }
+
+    @GetMapping("/{teamId}")
+    public ResponseEntity<List<RepoInvitation>> findAllByTeamId(@PathVariable String teamId) {
+        // TODO: return sorted list
+        List<RepoInvitation> result = repoInvitationRepository.findAllByTeamId(teamId);
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
